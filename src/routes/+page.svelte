089861dbc8c032bea86d09aa914a068f094609e0
Isaque Dantas <isaque.dantas@logap.com.br>
2026-02-26 10:06:47 -0300

    add

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<header class="flex p-6 justify-between bg-secondary/40 shadow-xs">
+    <p class="text-primary text-h5 font-mono! font-bold">Isaque Dantas</p>
+    <details>
+        <summary><span class="material-symbols-outlined text-white">menu</span></summary>
+        Content
+    </details>
+</header>
+
+<main class="flex flex-col gap-12 pt-24">
+    <div class="flex flex-col gap-6 items-center">
+        <h1 class="text-h3">I'm <strong class="text-h2 font-bold font-mono text-primary">Isaque Dantas</strong></h1>
+        <h3 class="text-h4 font-light">&gt; Full-stack developer</h3>
+        <p class="text-gray-1">I’m a full-stack developer in training, focused on building efficient, elegant, and scalable solutions.</p>
+    </div>
+    <div>
+
+    </div>
+
+</main>
+
+<footer>
+
+</footer>
